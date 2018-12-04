3d6b010ee202ed7edad797a94f7df137f4f83fac
LonghronShen <LonghronShen@hotmail.com>
2018-12-04 22:07:34 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -x
+
 upstream_ip=$1
 upstream_port=$2
 upstream_mode="${3:-socks5}"
 
+echo "$upstream_ip, $upstream_port, $upstream_mode"
+
 apt update && apt install -y redsocks gettext
 
 envsubst < redsocks.conf.template > /etc/redsocks.conf
+
+service redsocks restart
