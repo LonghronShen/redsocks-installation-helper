2123268ee69c36af3f072968c5adeda4dfb83551
LonghronShen <LonghronShen@hotmail.com>
2018-12-04 22:02:20 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-specificUser="${1:-root}"
-
-#不重定向目的地址为服务器的包
+# Prevent redirecting packs to the redsocks server.
 iptables -t nat -A OUTPUT -d 127.0.0.1 -j RETURN
 
-#不重定向私有地址的流量
+# Prevent redirecting to private addresses.
 iptables -t nat -A OUTPUT -d 10.0.0.0/8 -j RETURN
 iptables -t nat -A OUTPUT -d 172.16.0.0/16 -j RETURN
 iptables -t nat -A OUTPUT -d 192.168.0.0/16 -j RETURN
 
-#不重定向保留地址的流量,这一步很重要
+# Important! Prevent redirecting to preserved addresses.
 iptables -t nat -A OUTPUT -d 127.0.0.0/8 -j RETURN
 
-#重定向所有不满足以上条件的流量到redsocks监听的12345端口
+# Other packs should be redirected to the redsocks ports.
 iptables -t nat -A OUTPUT -p tcp -j REDIRECT --to-ports 12345
-
-# You can also control that in more precise way using `gid-owner` from
-# iptables.
-usermod --append --groups redsocks $specificUser
-iptables -t nat -A OUTPUT -p tcp -m owner --gid-owner redsocks -j REDSOCKS
